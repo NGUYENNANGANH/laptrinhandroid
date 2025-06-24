dae9ff01bc6bed69810b5fbaeb6e4e04d3e1c885
Thanh1417 <vuthithanh1417@gmail.com>
2025-06-24 15:49:21 +0700

    update activity_main.xml

--- app/src/main/java/com/example/truyenchu/MainActivity.java
+++ app/src/main/java/com/example/truyenchu/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.truyenchu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.truyenchu.Activities.SignupActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,11 +31,28 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button btn = findViewById(R.id.buttonHAHA);
+//        Button btn = findViewById(R.id.buttonHAHA);
 
-        btn.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-            startActivity(intent);
-        });
+//        btn.setOnClickListener(view -> {
+//            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+//            startActivity(intent);
+//        });
+        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+//        FloatingActionButton fabChat = findViewById(R.id.fab_chat);
+
+        // Lấy NavController từ NavHostFragment
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+
+        // Kết nối BottomNavigationView với NavController
+        // Thao tác này sẽ tự động xử lý việc chuyển fragment khi item được chọn
+        NavigationUI.setupWithNavController(navView, navController);
+
+//        fabChat.setOnClickListener(v -> {
+//            // TODO: Hiển thị popup chat ở đây
+//            Toast.makeText(MainActivity.this, "Mở popup chat!", Toast.LENGTH_SHORT).show();
+//        });
     }
 }
